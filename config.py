7ccb91f30ed57602f7d4b036ee5603acbb009127
alialomar246 <144565953+alialomar246@users.noreply.github.com>
2025-05-20 21:19:28 +0300

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "8010087659:AAHKI0K8nC243YwIrITUv8e_QsC2_81rOfI"
 
-DB_HOST = "http://46.235.15.27/admin.php"
+DB_HOST = "46.235.15.27"
 DB_USER = "admin"
 DB_PASS = "admin"
 DB_NAME = "radius"
