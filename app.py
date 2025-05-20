77ff045966e64781cee6a30ddd39c3b936753564
alialomar246 <144565953+alialomar246@users.noreply.github.com>
2025-05-20 13:34:05 +0300

    app.py

--- app.py
+++ app.py
@@ -46,3 +46,7 @@ port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
 def home():
     return "بوت Telegram Radius يعمل ✅"
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
